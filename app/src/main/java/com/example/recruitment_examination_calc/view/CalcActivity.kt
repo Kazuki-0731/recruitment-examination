38ecdf727879baea2cc072292f4fd3359f374af9
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2021-11-14 16:50:46 +0900

    delete viewmodel in CalcActivity

--- app/src/main/java/com/example/recruitment_examination_calc/view/CalcActivity.kt
+++ app/src/main/java/com/example/recruitment_examination_calc/view/CalcActivity.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.recruitment_examination_calc.R
 import com.example.recruitment_examination_calc.databinding.ActivityCalcBinding
-import com.example.recruitment_examination_calc.viewmodel.CalcViewModel
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
@@ -16,23 +14,13 @@ import javax.inject.Inject
 
 class CalcActivity : AppCompatActivity(), HasAndroidInjector {
     @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
-
-    @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
 
     lateinit var binding: ActivityCalcBinding
 
-    private lateinit var viewModel: CalcViewModel
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_calc)
-
-        viewModel = ViewModelProvider(
-            this,
-            viewModelFactory
-        ).get(CalcViewModel::class.java)
 
         replaceFragment(CalcFragment(), R.id.calc_content_view)
     }

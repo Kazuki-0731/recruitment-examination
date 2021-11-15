39e042db96ee2eae4280107b577f5f4dd3616912
Kazuki-0731 <28224709+Kazuki-0731@users.noreply.github.com>
2021-11-16 01:13:18 +0900

    add library in Dep.kt

--- buildSrc/src/main/java/com/example/recruitment_examination_calc/build/Dep.kt
+++ buildSrc/src/main/java/com/example/recruitment_examination_calc/build/Dep.kt
@@ -8,7 +8,8 @@ object Dep {
         const val fragment = "androidx.fragment:fragment-ktx:1.3.6"
 
         object Test {
-            const val ext = "androidx.test.ext:junit:1.1.3"
+            const val core = "androidx.test:core:1.4.0"
+            const val extJunit = "androidx.test.ext:junit:1.1.3"
             const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
             const val runner = "androidx.test:runner:1.4.0"
             const val rules = "androidx.test:rules:1.4.0"
@@ -20,11 +21,15 @@ object Dep {
         const val junit = "junit:junit:4.13.2"
     }
 
+    object Mockito {
+        const val core = "org.mockito:mockito-core:2.25.0"
+    }
+
     object Google {
         const val material = "com.google.android.material:material:1.4.0"
     }
 
-    object Jakewharton {
+    object Log {
         const val timber = "com.jakewharton.timber:timber:5.0.1"
     }
 
